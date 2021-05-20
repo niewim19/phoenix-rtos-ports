8bd88865f8623cd2a0f975cb6cfd667c74e13a76
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2021-05-20 17:50:26 +0200

    micropython: support for ARM arch
     - `build.sh` script recognizes architecture and compiles accordingly
     - additional compiler/linker flags set in `build.sh`
     - micropython binary moved to root/fs and prog.stripped
     - patches changed to incorporate stack and heap size flag
    
    JIRA: BES-145

--- micropython/build.sh
+++ micropython/build.sh
@@ -4,6 +4,7 @@ set -e
 
 UPYTH_VER="1.15"
 UPYTH="micropython-${UPYTH_VER}"
+UPYTH_CC_BASE=$(echo ${CC} | rev | cut -c4- | rev )
 
 PREFIX_UPYTH="${TOPDIR}/phoenix-rtos-ports/micropython"
 PREFIX_UPYTH_BUILD="${PREFIX_BUILD}/micropython"
@@ -14,17 +15,18 @@ b_log "Building micropython"
 pushd .
 cd $PREFIX_UPYTH
 
+
 #
 # Download
 #
 [ -f "${UPYTH}.tar.xz" ] || wget https://github.com/micropython/micropython/releases/download/v${UPYTH_VER}/${UPYTH}.tar.xz
-[ -d "${UPYTH}" ] || tar xf "${UPYTH}.tar.xz"
+[ -d "${UPYTH}" ] || ( echo "Unpacking ${UPYTH}.tar.xz..." && tar xf "${UPYTH}.tar.xz" )
 
 
 #
 # Patch
 #
-if ! patch -R -p0 -s -f --dry-run < ${PREFIX_UPYTH_PATCHSET}/lib.patch
+if ! patch -R -p0 -s -f --dry-run < ${PREFIX_UPYTH_PATCHSET}/lib.patch >/dev/null 2>&1
 then
 	patch -s -p0 < ${PREFIX_UPYTH_PATCHSET}/lib.patch
 	patch -s -p0 < ${PREFIX_UPYTH_PATCHSET}/py.patch
@@ -34,10 +36,38 @@ fi
 
 
 #
-# Build and move micropython binary to filesystem root/bin/
+# Setup flags for current architecture
 #
-cd ${UPYTH}/mpy-cross && make clean && make all BUILD=${PREFIX_UPYTH_BUILD} && cd ../..
-cd ${UPYTH}/ports/unix && make clean && make all
+if [[ ${UPYTH_CC_BASE} == "arm-phoenix-" ]]
+then
+	UPYTH_STACKSZ="4*512"
+	UPYTH_HEAPSZ="16*1024"
+	export LDFLAGS_EXTRA="-Wl,-q "
+	export CFLAGS_EXTRA="-nostartfiles -fpic -fpie -msingle-pic-base -mno-pic-data-is-text-relative -DUPYTH_STACKSZ=${UPYTH_STACKSZ} -DUPYTH_HEAPSZ=${UPYTH_HEAPSZ}"
+	export STRIPFLAGS_EXTRA="--strip-unneeded"
+elif [[ ${UPYTH_CC_BASE} == "i386-pc-phoenix-" ]]
+then
+	UPYTH_STACKSZ="40000"
+	UPYTH_HEAPSZ="1024*1024"
+	export CFLAGS_EXTRA="-DUPYTH_STACKSZ=${UPYTH_STACKSZ} -DUPYTH_HEAPSZ=${UPYTH_HEAPSZ}"
+	export STRIPFLAGS_EXTRA="--strip-unneeded"
+else
+	echo "Error: there is no micropython port for chosen architecture"
+fi
+
+
+#
+# Build 
+#
+cd ${UPYTH}/mpy-cross && make clean && make all BUILD=${PREFIX_UPYTH_BUILD} CROSS_COMPILE=${UPYTH_CC_BASE} && cd ../..
+cd ${UPYTH}/ports/unix && make clean && make all CROSS_COMPILE=${UPYTH_CC_BASE} PHOENIX_LD=${UPYTH_VER}
+${UPYTH_CC_BASE}strip ${STRIPFLAGS_EXTRA} micropython
+
+
+#
+# Move binary
+#
 mkdir -p "${PREFIX_FS}/root/bin/" && cp micropython "${PREFIX_FS}/root/bin/"
+cp micropython ${PREFIX_PROG_STRIPPED}
 
 popd
